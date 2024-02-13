f83d8fa2d1619ee4abe924afd7eaed296690dd7f
jspmern <jspmern@gmail.com>
2024-02-13 19:28:39 +0530

    new product schema

--- model/productModel.js
+++ model/productModel.js
@@ -8,5 +8,33 @@ let productSchema= new mongoose.Schema({
         type:String,
         lowercase:true,
         require:true
+    },
+    price:{
+        type:Number,
+        require:true
+    },
+    quantity:{
+        type:Number,
+        require:true
+    },
+    brand:{
+        type:String,
+    },
+    description:{
+        type:String,
+        require:true
+    },
+    images:[{
+        type:String,
+        require:true
+    }],
+    category:{
+         type:mongoose.ObjectId,
+         ref:"category",
+         require:true
+    },
+    shipping:{
+        type:String,
+        default:'yes'
     }
 },{timestamps:true})
