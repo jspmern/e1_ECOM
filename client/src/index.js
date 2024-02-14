18fbd29a178a484f51f409eb72a785deda82db62
jspmern <jspmern@gmail.com>
2024-02-14 18:15:59 +0530

    restart

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
  
-
+//this is for testing
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
